85f797f61f12adf99d981e2ab4cd9088e87932c3
malasiot <malasiot@iti.gr>
2017-09-05 15:22:46 +0300

    mustache extensions

--- include/wspp/util/template_renderer.hpp
+++ include/wspp/util/template_renderer.hpp
@@ -37,13 +37,14 @@ public:
 
     TemplateRenderer( const boost::shared_ptr<TemplateLoader> &loader, bool caching = false ): loader_(loader), caching_(caching) {}
 
-    // Render a template file or template string using given context and list of partials.
-    // If the resource is a file the string has to be prefixed by '@' which is relative to the root folder.
-    // "partials" is a map of key value pairs i.e. mapping partial keys in the template to actual resources (template string or files)
-    // For convenience if a partial key starts with @ then corresponding resource will be @key
+    // Render a template using given context and list of partials.
+    // The loader is used to map the given key to a file or other resource.
     // A global cache is used to store compiled templates if caching is enabled.
-    // A simple form of dynamic partials is implemented by allowing partial keys to contain substitution patterns e.g. {{> @forms/%field_type%_field.mst }}
-    // The pattern params are looked up in the context stack and replaced with corresponding values before rendering the partials.
+    //
+    // A simple form of dynamic partials is implemented by testing of the key is a variable in the current context. The corresponding value is then used as partial key.
+    // Partials can take arguments in the form of "{{> <name> <arg1>="<val1>" <arg2>="<val2>" ... }}". The arguments are pushed in the context stack before evaluting the partial.
+    // The implementation also supports an extension of mustache namely extensions and blocks similar to
+    // https://github.com/mustache/spec/issues/38
     // Also the substitution pattern {{.}} is used to render the current array element in an array section (in this case the data object should point to an array of literals)
     // Lambdas are not supported.
 
